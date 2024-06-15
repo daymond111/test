308219016cf5705a39376ee22ec1fe790cbc65a1
daymond111 <100313745+daymond111@users.noreply.github.com>
2024-06-14 22:33:56 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,41 +1,51 @@
-import sys
 import os
+import sys
 
 BUILT_IN_COMMANDS = ['echo', 'exit', 'type']
+PATHS = os.environ['PATH'].split(os.pathsep)
 
-def prompt_user():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-    return input()
+def get_user_input():
+    return input('$ ')
 
-def execute_command(command):
-    cmd = command[5:]
-    PATHS = os.environ.get("PATH").split(os.pathsep)
-    command_path = None
+def find_program_path(command):
+    for path in PATHS:
+        program_path = os.path.join(path, command)
+        if os.path.isfile(program_path):
+            return program_path
+    return None
 
+def execute_built_in_command(command, args):
     match command:
-        case "exit 0":
+        case 'exit':
             sys.exit(0)
-        case command if command.startswith("echo "):
-            print(cmd)
-        case command if command.startswith("type "):
-            for path in PATHS:
-                if os.path.isfile(os.path.join(path, cmd)):
-                    command_path = os.path.join(path, cmd)
-                    break
-            if cmd in BUILT_IN_COMMANDS:
-                print(f"{cmd} is a shell builtin")
-            elif command_path:
-                print(f"{cmd} is {command_path}")
+        case 'echo':
+            print(' '.join(args))
+        case 'type':
+            if args[0] in BUILT_IN_COMMANDS:
+                print(f"{args[0]} is a shell builtin")
             else:
-                print(f"{cmd}: not found")
-        case _:
-            print(f"{command}: command not found")
+                program_path = find_program_path(args[0])
+                if program_path:
+                    print(f"{args[0]} is {program_path}")
+                else:
+                    print(f"{args[0]}: not found")
+
+def execute_external_command(command, args):
+    program_path = find_program_path(command)
+    if program_path:
+        os.system(f'{program_path} {" ".join(args)}')
+    else:
+        print(f'{command}: command not found')
 
 def main():
     while True:
-        command = prompt_user()
-        execute_command(command)
+        command = get_user_input()
+        command_parts = command.split()
+        if command_parts:
+            if command_parts[0] in BUILT_IN_COMMANDS:
+                execute_built_in_command(command_parts[0], command_parts[1:])
+            else:
+                execute_external_command(command_parts[0], command_parts[1:])
 
 if __name__ == "__main__":
     main()
